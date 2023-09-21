10cc52489214d6f64d759d1607511f4e5ab9d71b
Julian Abouzeid <Julian@wheresnick.com.au>
2023-09-21 22:04:54 +1000

    added yesterdays work which was added date and button listener

--- Assets/style/jss/script.js
+++ Assets/style/jss/script.js
@@ -1,7 +1,25 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+
+currentDayElement.textContent = currentDate;
+
+
+// current time //
 $(function () {
+  var currentDay = dayjs().format("dddd, MMMM D, YYYY");
+  var currentDayElement = document.getElementById("currentDay")
+  currentDayElement.textContent(currentDay);
+});
+
+// save button event listener //
+$(".saveBtn").on("click", function () {
+  var textarea = $(this).siblings(".description");
+  var userDescription = textarea.val();
+  var timeBlockId = $(this).parent().attr("id");
+
+  localStorage.setItem(timeBlockId, userDescription);
+});
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +38,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
